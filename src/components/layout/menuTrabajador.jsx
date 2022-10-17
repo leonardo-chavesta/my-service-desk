411735788c0108d405b119a609a9a64ba1ceb03d
leonardo-chavesta <104604073+leonardo-chavesta@users.noreply.github.com>
2022-10-17 14:15:14 -0500

    agregando contenido

--- src/components/layout/menuTrabajador.jsx
+++ src/components/layout/menuTrabajador.jsx
@@ -15,6 +15,7 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Detalle from "../../views/Detalle/detalle"
 
 const LayoutPanelDeControl = () => {
   return (
@@ -50,6 +51,8 @@ const LayoutPanelDeControl = () => {
           </div>
         </header>
       </div>
+
+      <Detalle />
     </>
   );
 };
--- src/views/Detalle/detalle.jsx
+++ src/views/Detalle/detalle.jsx
@@ -1,4 +1,46 @@
-import '../../App.css'
+import "../../App.css";
+import Button from "react-bootstrap/Button";
+import { Row, Col, Form } from "react-bootstrap";
 
-const Detalle = () => <h1 className="articulo">Soy el detalle</h1>
+const Detalle = () => {
+  return (
+    <>
+      <Row className="g-3">
+       
+        <Col xs={12} sm={4} md={6} lg={6} xxl={6}>
+          <Form.Label>Jefe de Area de Servicio al Administrado</Form.Label>
+          <Form.Control size="sm" name="jefeArea" type="text" />
+        </Col>
+        <Col xs={12} sm={6} md={6} lg={6} xxl={6}>
+          <Form.Label>Especialista en Certificaciones</Form.Label>
+          <Form.Control size="sm" as='textarea' readOnly />
+        </Col>
+        <Col xs={12} sm={4} md={6} lg={6} xxl={6}>
+          <Form.Label>Jefe de Area de Servicio al Administrado</Form.Label>
+          <Form.Control size="sm" name="jefeArea" type="text" />
+        </Col>
+        <Col xs={12} sm={6} md={6} lg={6} xxl={6}>
+          <Form.Label>Especialista en Certificaciones</Form.Label>
+          <Form.Control size="sm" as='textarea' readOnly />
+        </Col>
+        <Col xs={12} sm={4} md={6} lg={6} xxl={6}>
+          <Form.Label>Jefe de Area de Servicio al Administrado</Form.Label>
+          <Form.Control size="sm" name="jefeArea" type="text" />
+        </Col>
+        <Col xs={12} sm={6} md={6} lg={6} xxl={6}>
+          <Form.Label>Especialista en Certificaciones</Form.Label>
+          <Form.Control size="sm" as='textarea' readOnly />
+        </Col>
+        <Col xs={12} sm={4} md={6} lg={6} xxl={6}>
+          <Form.Label>Jefe de Area de Servicio al Administrado</Form.Label>
+          <Form.Control size="sm" name="jefeArea" type="text" />
+        </Col>
+        <Col xs={12} sm={6} md={6} lg={6} xxl={6}>
+          <Form.Label>Especialista en Certificaciones</Form.Label>
+          <Form.Control size="sm" as='textarea' readOnly />
+        </Col>
+      </Row>
+    </>
+  );
+};
 export default Detalle;
