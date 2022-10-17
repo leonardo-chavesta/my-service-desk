2bce41c463a320aa7141da5dbff132e417fc8d5e
leonardo-chavesta <104604073+leonardo-chavesta@users.noreply.github.com>
2022-10-17 12:05:26 -0500

    estructurando la pagina

--- src/components/MenuPrincilpal.jsx
+++ src/components/MenuPrincilpal.jsx
@@ -1,53 +1,48 @@
-import imgMenu from "../templates/img/menu-img.jpg";
-import menuLogo from "../templates/img/data.jpg";
-import Hor from "../views/home/inicio" 
 import "../templates/styles/nav.css";
-
+import Home from "../views/home/home";
+import { Route, Link, Routes, NavLink, useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const MenuPrincipal = () => {
-
-const alert = () => {
-  console.log("HOla");
-}
-
+  const alert = () => {
+    console.log("Hola");
+  };
 
 
-return (
+
+  return (
     <>
       <div className="contenedor">
         <div className="header__container">
           <div className="header_nav">
-            <ul className="header_ul">
-              <li>Titulo</li>
-              <li>Titulo</li>
-              <li>Titulo</li>
-              <li>Titulo</li>
+            <ul className="header_ul ">
+              <li></li>
+              <li></li>
             </ul>
           </div>
           <div className="header_nav">
-            <ul className="header_ul ">
-              <li>Login</li>
-              <li>Sign Up</li>
+            <ul className="header_ul">
+              <li>
+                <Link to="">Inicio</Link>
+              </li>
+              <li>
+                <NavLink to="/usuario">Acerca De Nosotros</NavLink>
+              </li>
+              <li>
+                <a>Servicios</a>
+              </li>
+              <li>
+                <Link to="/login">
+                  <Button variant="danger" active >
+                    Login Servicio
+                  </Button>
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
-
-
       </div>
-      <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     <Hor />
-     
-
+      <Home />
     </>
   );
 };
---
+++ src/views/Detalle/detalle.jsx
@@ -0,0 +1,4 @@
+import '../../App.css'
+
+const Detalle = () => <h1 className="articulo">Soy el detalle</h1>
+export default Detalle;
